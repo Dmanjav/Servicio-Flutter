b3f6c86c71f4ce39ce06c194eadb8ade87344a81
Dmanjav <A01753486@tec.mx>
2023-07-13 14:08:09 -0600

    primer ajuste
    solo le cambié el nombre a la pafina

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -31,7 +31,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Pagina Nueva'),
+      home: const MyHomePage(title: 'modificacion inmediata?'),
     );
   }
 }
