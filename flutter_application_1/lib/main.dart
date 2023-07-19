1b45596b026a1ce54e4e675605b2baf158bf8263
Dmanjav <A01753486@tec.mx>
2023-07-18 20:07:51 -0600

    se acabo el tutorial
    buscar como cambiar estilos

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -11,7 +11,7 @@
 //   ));
 // }
 
-// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, prefer_collection_literals, unused_local_variable
 
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
@@ -23,7 +23,9 @@ class MyApp extends StatelessWidget {
   //Para este snippet usar "build"
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Welcome to Flutter',
+      theme: ThemeData(primaryColor: Colors.white),
       home: RandomWords(),
     );
   }
@@ -38,30 +40,83 @@ class RandomWords extends StatefulWidget {
 
 class RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
+  final _saved = Set<WordPair>();
+  final _biggerFont = const TextStyle(fontSize: 18.0);
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Palabras Random Nuevas'),
+        actions: <Widget>[
+          IconButton(onPressed: _pushSaved, icon: Icon(Icons.list))
+        ],
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(MaterialPageRoute(builder: (context) {
+      final tiles = _saved.map((pair) {
+        return ListTile(
+          title: Text(
+            pair.asPascalCase,
+            style: _biggerFont,
+          ),
+        );
+      });
+
+      final divided =
+          ListTile.divideTiles(context: context, tiles: tiles).toList();
+      return Scaffold(
         appBar: AppBar(
-          title: Text('Palabras Random Nuevas'),
+          title: Text('Saved Suggestions'),
         ),
-        body: _buildSuggestions(), 
+        body: ListView(
+          children: divided,
+        ),
       );
+    }));
   }
 
   Widget _buildSuggestions() {
     return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
       itemBuilder: (context, i) {
+        if (i.isOdd) {
+          return Divider();
+        }
+
         if (i >= _suggestions.length) {
           _suggestions.addAll(generateWordPairs().take(10));
         }
-        return _buildRow(_suggestions[i]);
+        final index = i ~/ 2;
+
+        return _buildRow(_suggestions[index]);
       },
     );
   }
 
   Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
     return ListTile(
-      title: Text(pair.asPascalCase),
+      title: Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
     );
   }
 }
