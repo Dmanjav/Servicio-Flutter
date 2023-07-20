21f7ff9f3e938a60df13beac14f3c831f2338275
Dmanjav <A01753486@tec.mx>
2023-07-19 20:15:14 -0600

    20% hechp
    agregar los documentos, no sé hacer eso

--- componentes/nodo_control/lib/main.dart
+++ componentes/nodo_control/lib/main.dart
@@ -15,7 +15,7 @@ class MyApp extends StatelessWidget {
       title: 'Detalles Camionero',
       theme: ThemeData(
         colorSchemeSeed: Color.fromARGB(255, 73, 118, 232),
-        scaffoldBackgroundColor: Colors.white,
+        scaffoldBackgroundColor: Color.fromARGB(255, 247, 249, 251),
         useMaterial3: false,
       ),
       home: Cards(),
@@ -70,79 +70,151 @@ Widget nodooControl() {
           textStyle: TextStyle(fontSize: 22.0, fontWeight: FontWeight.bold),
         ),
       ),
-      
       Card(
-        color: Colors.white,
-        child: Row(
-          children: <Widget>[
-            Expanded(child: Container(
-                child: Row(
-              children: [ 
-                IconButton(
-                  icon: Icon(
-                    Icons.people_outline_outlined,
-                    color: Color.fromARGB(255, 170, 197, 242),
-                    size: 30,
+          color: Colors.white,
+          child: Column(
+            children: [
+              Row(
+                children: <Widget>[
+                  Expanded(
+                    child: Container(
+                        child: Row(
+                      children: [
+                        IconButton(
+                          icon: Icon(
+                            Icons.people_outline_outlined,
+                            color: Color.fromARGB(255, 170, 197, 242),
+                            size: 30,
+                          ),
+                          onPressed: () {},
+                        ),
+                        Column(
+                          children: <Widget>[
+                            Text(
+                              '200+',
+                              style: GoogleFonts.raleway(
+                                  textStyle: TextStyle(
+                                color: Color.fromARGB(255, 32, 78, 226),
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold,
+                              )),
+                            ),
+                            Text('Conexiones',
+                                style: GoogleFonts.raleway(
+                                    textStyle: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 16,
+                                )))
+                          ],
+                        ),
+                      ],
+                    )),
                   ),
-                  onPressed: () {},
-                ),
-                Column(
-                  children: <Widget>[
-                    Text(
-                      '200+',
-                      style: GoogleFonts.raleway(
-                          textStyle: TextStyle(
-                        color: Color.fromARGB(255, 32, 78, 226),
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                      )),
-                    ),
-                    Text('Conexiones',
+                  Expanded(
+                      child: Container(
+                          child: Row(
+                    children: [
+                      IconButton(
+                        icon: Icon(
+                          Icons.local_shipping_outlined,
+                          color: Color.fromARGB(255, 170, 197, 242),
+                          size: 30,
+                        ),
+                        onPressed: () {},
+                      ),
+                      Column(
+                        children: <Widget>[
+                          Text('500+',
+                              style: GoogleFonts.raleway(
+                                  textStyle: TextStyle(
+                                color: Color.fromARGB(255, 32, 78, 226),
+                                fontSize: 24,
+                                fontWeight: FontWeight.bold,
+                              )),
+                              textAlign: TextAlign.left),
+                          Text('Fletes Hechos',
+                              style: GoogleFonts.raleway(
+                                  textStyle: TextStyle(
+                                color: Colors.black,
+                                fontSize: 16,
+                              )))
+                        ],
+                      )
+                    ],
+                  ))),
+                ],
+              ),
+              Divider(
+                indent: 15.0,
+                endIndent: 15.0,
+                color: Color.fromARGB(255, 150, 150, 150),
+              ),
+              Row(
+                children: <Widget>[
+                  Container(
+                      child: Row(
+                    children: [
+                      IconButton(
+                        icon: Icon(
+                          Icons.folder_shared_outlined,
+                          color: Color.fromARGB(255, 150, 150, 150),
+                          size: 20,
+                        ),
+                        onPressed: () {},
+                      ),
+                      Text(
+                        'Validación',
                         style: GoogleFonts.raleway(
                             textStyle: TextStyle(
                           color: Colors.black,
-                          fontSize: 16,
-                        )))
-                  ],
-                ),
-              ],
-            )),),
-            Expanded(child: Container(
-                child: Row(
-              children: [
-                IconButton(
-                  icon: Icon(
-                    Icons.local_shipping_outlined,
-                    color: Color.fromARGB(255, 170, 197, 242),
-                    size: 30,
-                  ),
-                  onPressed: () {},
-                ),
-                Column(
-                  children: <Widget>[
-                    Text(
-                      '500+',
-                      style: GoogleFonts.raleway(
-                          textStyle: TextStyle(
-                        color: Color.fromARGB(255, 32, 78, 226),
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
-                      )),
-                    textAlign: TextAlign.left
+                          fontSize: 15,
+                          fontWeight: FontWeight.bold,
+                        )),
+                      ),
+                    ],
+                  )),
+                  Container(
+                    margin: EdgeInsets.only(left: 12),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Color.fromARGB(255, 198, 250, 201),
                     ),
-                    Text('Fletes Hechos',
-                        style: GoogleFonts.raleway(
-                            textStyle: TextStyle(
-                          color: Colors.black,
-                          fontSize: 16,
-                        )))
-                  ],
-                )
-              ],
-            ))),
-          ],
-        ),
-      ),
+                    height: 20,
+                    width: 50,
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.lock_outline,
+                          color: Color.fromARGB(255, 150, 150, 150),
+                          size: 15,
+                        ),
+                        Text(
+                          ' 5/5',
+                          style: GoogleFonts.raleway(
+                              textStyle: TextStyle(
+                            color: Colors.black,
+                            fontSize: 15,
+                          )),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Expanded(
+                      child: Container(
+                        alignment: Alignment.centerRight,
+                    child: IconButton(
+                      icon: Icon(
+                        Icons.keyboard_arrow_down,
+                        color: Color.fromARGB(255, 150, 150, 150),
+                        size: 25,
+                      ),
+                      onPressed: () {},
+                    ),
+                  ))
+                ],
+              ),
+            ],
+          )),
       ElevatedButton(
         style: raisedButtonStyle,
         child: Row(
