da233160703af5faa107b4a76fde02b2ef85d975
Diego Manja <94866755+Dmanjav@users.noreply.github.com>
2023-07-19 18:36:36 -0600

    ya quedó la primera parte
    falta terminar más de la mitad

--- componentes/nodo_control/lib/main.dart
+++ componentes/nodo_control/lib/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: use_key_in_widget_constructors, prefer_const_literals_to_create_immutables, prefer_const_constructors
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_literals_to_create_immutables, prefer_const_constructors, avoid_unnecessary_containers
 
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -42,7 +42,6 @@ class CardsState extends State<Cards> {
           ),
         ),
       ),
-      
       body: nodooControl(),
       /* Column(
         children: [
@@ -59,6 +58,7 @@ Widget nodooControl() {
       backgroundColor: Color.fromARGB(255, 32, 226, 165),
       alignment: Alignment.center,
       elevation: 3.0,
+      maximumSize: Size(100, 50),
       padding: EdgeInsets.all(5.0));
 
   return ListView(
@@ -70,12 +70,79 @@ Widget nodooControl() {
           textStyle: TextStyle(fontSize: 22.0, fontWeight: FontWeight.bold),
         ),
       ),
-
+      
       Card(
         color: Colors.white,
-        child: Text('This is a card'),
+        child: Row(
+          children: <Widget>[
+            Expanded(child: Container(
+                child: Row(
+              children: [ 
+                IconButton(
+                  icon: Icon(
+                    Icons.people_outline_outlined,
+                    color: Color.fromARGB(255, 170, 197, 242),
+                    size: 30,
+                  ),
+                  onPressed: () {},
+                ),
+                Column(
+                  children: <Widget>[
+                    Text(
+                      '200+',
+                      style: GoogleFonts.raleway(
+                          textStyle: TextStyle(
+                        color: Color.fromARGB(255, 32, 78, 226),
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      )),
+                    ),
+                    Text('Conexiones',
+                        style: GoogleFonts.raleway(
+                            textStyle: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                        )))
+                  ],
+                ),
+              ],
+            )),),
+            Expanded(child: Container(
+                child: Row(
+              children: [
+                IconButton(
+                  icon: Icon(
+                    Icons.local_shipping_outlined,
+                    color: Color.fromARGB(255, 170, 197, 242),
+                    size: 30,
+                  ),
+                  onPressed: () {},
+                ),
+                Column(
+                  children: <Widget>[
+                    Text(
+                      '500+',
+                      style: GoogleFonts.raleway(
+                          textStyle: TextStyle(
+                        color: Color.fromARGB(255, 32, 78, 226),
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      )),
+                    textAlign: TextAlign.left
+                    ),
+                    Text('Fletes Hechos',
+                        style: GoogleFonts.raleway(
+                            textStyle: TextStyle(
+                          color: Colors.black,
+                          fontSize: 16,
+                        )))
+                  ],
+                )
+              ],
+            ))),
+          ],
+        ),
       ),
-      
       ElevatedButton(
         style: raisedButtonStyle,
         child: Row(
@@ -89,17 +156,15 @@ Widget nodooControl() {
         ),
         onPressed: () {},
       ),
-
       Text(
         'Tarifa de Servicio',
         style: GoogleFonts.raleway(
           textStyle: TextStyle(fontSize: 22.0, fontWeight: FontWeight.bold),
         ),
       ),
-
       Card(
         color: Colors.white,
-        child: Text('This is one more card'),
+        child: Text('No puedo, esto está muy dificil'),
       ),
     ],
   );
