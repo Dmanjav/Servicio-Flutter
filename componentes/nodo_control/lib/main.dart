8ce4fd33e37144190b59a47a9bfad29c699eb91b
Dmanjav <A01753486@tec.mx>
2023-07-24 16:37:01 -0600

    finalizado
    solo faltó el nodo control

--- componentes/nodo_control/lib/main.dart
+++ componentes/nodo_control/lib/main.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: use_key_in_widget_constructors, prefer_const_literals_to_create_immutables, prefer_const_constructors, avoid_unnecessary_containers, no_leading_underscores_for_local_identifiers, prefer_const_declarations, prefer_final_fields
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_literals_to_create_immutables, prefer_const_constructors, avoid_unnecessary_containers, no_leading_underscores_for_local_identifiers, prefer_const_declarations, prefer_final_fields, dead_code
 
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
@@ -35,13 +35,110 @@ class CardsState extends State<Cards> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          'Alejando Perez',
-          style: GoogleFonts.raleway(
-            textStyle: TextStyle(fontSize: 22.0, fontWeight: FontWeight.bold),
+          toolbarHeight: 100,
+          backgroundColor: Color.fromARGB(255, 73, 118, 232),
+          title: Row(
+            children: [
+              Wrap(
+                spacing: 20,
+                crossAxisAlignment: WrapCrossAlignment.center,
+                children: [
+                  Container(
+                      child: Image.asset(
+                    'assets/images/profile.png',
+                    height: 60,
+                  )),
+                  Expanded(
+                      child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text("Alejandro Perez",
+                          style: GoogleFonts.raleway(
+                              textStyle: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ))),
+                      Text("Proveedor",
+                          style: GoogleFonts.raleway(
+                              textStyle: TextStyle(
+                            fontSize: 18,
+                            color: Color.fromARGB(255, 247, 249, 251),
+                          ))),
+                    ],
+                  )), 
+                ],
+              ),
+              Expanded(
+                  child: Column(
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(left: 12),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Color.fromARGB(255, 237, 243, 254),
+                    ),
+                    height: 20,
+                    width: 50,
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.shield_sharp,
+                          color: Color.fromARGB(255, 73, 118, 232),
+                          size: 15,
+                        ),
+                        Text(
+                          ' 80%',
+                          style: GoogleFonts.raleway(
+                              textStyle: TextStyle(
+                            color: Colors.black,
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                          )),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(height: 10),
+                  Container(
+                    margin: EdgeInsets.only(left: 12),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Color.fromARGB(255, 251, 235, 188),
+                    ),
+                    height: 20,
+                    width: 50,
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.star_rounded,
+                          color: Colors.yellow[700],
+                          size: 15,
+                        ),
+                        Text(
+                          ' 4.8',
+                          style: GoogleFonts.raleway(
+                              textStyle: TextStyle(
+                            color: Colors.black,
+                            fontSize: 15,
+                            fontWeight: FontWeight.bold,
+                          )),
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ))
+            ],
           ),
-        ),
-      ),
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage("assets/images/lines.png"),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),),
       body: nodooControl(),
       /* Column(
         children: [
@@ -61,6 +158,7 @@ Widget nodooControl() {
       padding: EdgeInsets.all(5.0),
       maximumSize: Size(100, 50));
 
+  bool showMore = false;
 
   return ListView(
     padding: const EdgeInsets.all(20.0),
@@ -71,6 +169,7 @@ Widget nodooControl() {
           textStyle: TextStyle(fontSize: 22.0, fontWeight: FontWeight.bold),
         ),
       ),
+      SizedBox(height: 10),
       Card(
           elevation: 5.0,
           color: Colors.white,
@@ -91,6 +190,7 @@ Widget nodooControl() {
                           onPressed: () {},
                         ),
                         Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
                           children: <Widget>[
                             Text(
                               '200+',
@@ -124,6 +224,7 @@ Widget nodooControl() {
                         onPressed: () {},
                       ),
                       Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
                         children: <Widget>[
                           Text('500+',
                               style: GoogleFonts.raleway(
@@ -177,7 +278,7 @@ Widget nodooControl() {
                     margin: EdgeInsets.only(left: 12),
                     decoration: BoxDecoration(
                       borderRadius: BorderRadius.circular(10),
-                      color: Color.fromARGB(255, 204, 255, 230  ),
+                      color: Color.fromARGB(255, 204, 255, 230),
                     ),
                     height: 20,
                     width: 50,
@@ -203,12 +304,15 @@ Widget nodooControl() {
                       child: Container(
                     alignment: Alignment.centerRight,
                     child: IconButton(
-                      icon: Icon(
-                        Icons.keyboard_arrow_down,
-                        color: Color.fromARGB(255, 150, 150, 150),
-                        size: 25,
-                      ),
-                      onPressed: () {
+                        icon: Icon(
+                          showMore
+                              ? Icons.keyboard_arrow_down
+                              : Icons.keyboard_arrow_up,
+                          color: Color.fromARGB(255, 150, 150, 150),
+                          size: 25,
+                        ),
+                        onPressed:
+                            () {} //=> setState (() => showMore = !showMore),
                         /* 
 
 
@@ -216,8 +320,7 @@ Widget nodooControl() {
 
 
                          */
-                      },
-                    ),
+                        ),
                   )),
                 ],
               ),
@@ -384,6 +487,7 @@ Widget nodooControl() {
               ),
             ],
           )),
+      SizedBox(height: 10),
       ElevatedButton.icon(
 /* No le puedo cambiar el tamaño al boton, no se porqué agh, dios mio*/
         icon: Icon(Icons.phone_forwarded_outlined),
@@ -391,12 +495,14 @@ Widget nodooControl() {
         style: raisedButtonStyle,
         label: Text('Llamar'),
       ),
+      SizedBox(height: 10),
       Text(
         'Tarifa de Servicio',
         style: GoogleFonts.raleway(
           textStyle: TextStyle(fontSize: 22.0, fontWeight: FontWeight.bold),
         ),
       ),
+      SizedBox(height: 10),
       Card(
         elevation: 5.0,
         color: Colors.white,
@@ -406,57 +512,197 @@ Widget nodooControl() {
   );
 }
 
+
+//Codigo a continuación para la cantidad de requisitos incumplidos
+
+                  /* Container(
+                    margin: EdgeInsets.only(left: 12),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(10),
+                      color: Color.fromARGB(255, 248, 214, 223),
+                    ),
+                    height: 20,
+                    width: 50,
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.lock_outline,
+                          color: Colors.red,
+                          size: 15,
+                        ),
+                        Text(
+                          ' 0/5',
+                          style: GoogleFonts.raleway(
+                              textStyle: TextStyle(
+                            color: Colors.red,
+                            fontSize: 15,
+                          )),
+                        ),
+                      ],
+                    ),
+                  ), */
+
+
+//El codigo a continuación es el que se debe mostrar al hacer click en la flecha y algún requisito no se cumple.
+
 /*
-
-Esto es lo que se muestra trase presionar el botón de la flecha
-
-
-
- class ExpandedInfo extends StatefulWidget {
-  @override
-  State<StatefulWidget> createState() {
-    return ExpandedInfoState();
-  }
-}
-
-class ExpandedInfoState extends State<ExpandedInfo> {
-  bool _visible = false;
-
-  @override
-  void initState() {
-    super.initState();
-    _visible = false;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onPressed: () {
-        setState(() {
-          _visible = !_visible;
-        });
-      },
-    );
-  }
-}
- */
-
-
-/* 
-Widget tarifaServicio() {
-  return ListView(
-    padding: const EdgeInsets.all(20.0),
-    children: <Widget>[
-      Text(
-        'Tarifa de Servicio',
-        style: GoogleFonts.raleway(
-          textStyle: TextStyle(fontSize: 22.0, fontWeight: FontWeight.bold),
-        ),
-      ),
-      Card(
-        color: Colors.white,
-        child: Text('This is one more card'),
-      ),
-    ],
-  );
-} */
+              Container(
+                margin: EdgeInsets.all(15),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Text('INE ',
+                            style: GoogleFonts.raleway(
+                                textStyle: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ))),
+                        Icon(
+                          Icons.block,
+                          color: Colors.red,
+                          size: 18,
+                        ),
+                      ],
+                    ),
+                    Text(
+                      "La identidad de esta persona no ha sido validada.",
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 2,
+                      style: GoogleFonts.raleway(
+                          textStyle: TextStyle(
+                        fontSize: 16,
+                        color: Color.fromARGB(255, 150, 150, 150),
+                      )),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(15),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Text('Tarjeta de Circulación ',
+                            style: GoogleFonts.raleway(
+                                textStyle: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ))),
+                        Icon(
+                          Icons.block,
+                          color: Colors.red,
+                          size: 18,
+                        ),
+                      ],
+                    ),
+                    Text(
+                      "Esta persona no cumple con los requisitos de mobilidad vigentes.",
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 2,
+                      style: GoogleFonts.raleway(
+                          textStyle: TextStyle(
+                        fontSize: 16,
+                        color: Color.fromARGB(255, 150, 150, 150),
+                      )),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(15),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Text('Tarjeta de Tracto ',
+                            style: GoogleFonts.raleway(
+                                textStyle: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ))),
+                        Icon(
+                          Icons.block,
+                          color: Colors.red,
+                          size: 18,
+                        ),
+                      ],
+                    ),
+                    Text(
+                      "El vehículo no tiene sus papeles al día y vigencia inválida.",
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 2,
+                      style: GoogleFonts.raleway(
+                          textStyle: TextStyle(
+                        fontSize: 16,
+                        color: Color.fromARGB(255, 150, 150, 150),
+                      )),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(15),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Text('Poliza de Seguro del Tracto ',
+                            style: GoogleFonts.raleway(
+                                textStyle: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ))),
+                        Icon(
+                          Icons.block,
+                          color: Colors.red,
+                          size: 18,
+                        ),
+                      ],
+                    ),
+                    Text(
+                      "Esta persona no cumple con los requisitos de mobilidad vigentes.",
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 2,
+                      style: GoogleFonts.raleway(
+                          textStyle: TextStyle(
+                        fontSize: 16,
+                        color: Color.fromARGB(255, 150, 150, 150),
+                      )),
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                margin: EdgeInsets.all(15),
+                child: Column(
+                  children: [
+                    Row(
+                      children: [
+                        Text('Licencia del Operador ',
+                            style: GoogleFonts.raleway(
+                                textStyle: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                            ))),
+                        Icon(
+                          Icons.block,
+                          color: Colors.red,
+                          size: 18,
+                        ),
+                      ],
+                    ),
+                    Text(
+                      "Esta persona no cumple con los requisitos de mobilidad vigentes.",
+                      overflow: TextOverflow.ellipsis,
+                      maxLines: 2,
+                      style: GoogleFonts.raleway(
+                          textStyle: TextStyle(
+                        fontSize: 16,
+                        color: Colors.fromARGB(255, 150, 150, 150),
+                      )),
+                    ),
+                  ],
+                ),
+              ),*/
